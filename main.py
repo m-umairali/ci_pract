ab13c9f78d6ba8886935a624d089b465776b7c4f
umair <umairalism@gmail.com>
2023-12-15 23:29:35 +0500

    hf

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 
 fig, ax = plt.subplots()
 
-fruits = ['applee', 'blueberdry', 'cherry', 'orange']
+fruits = ['apple', 'blueberdry', 'cherry', 'orange']
 counts = [140, 10, 30, 55]
 bar_labels = ['red', 'blue', '_red', 'orange']
 bar_colors = ['tab:red', 'tab:blue', 'tab:red', 'tab:orange']
